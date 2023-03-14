3602a5b672e1bf778757362b58bcd74741203bfc
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2023-03-14 12:30:24 +0600

    get image

--- Image To Text/Screens/Home/View/HomeViewController.swift
+++ Image To Text/Screens/Home/View/HomeViewController.swift
@@ -6,24 +6,53 @@
 //
 
 import UIKit
+import Vision
+import PhotosUI
 
 class HomeViewController: UIViewController {
-
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var showTextLabel: UILabel!
+    @IBOutlet weak var getTextButton: UIButton!
+    @IBOutlet weak var getPhotoButton: UIButton!
+    
+    var imageArray = UIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - IBAction
+    @IBAction func getTextButtonAction(_ sender: UIButton) {
+        print("Get Text Button")
     }
-    */
-
+    
+    @IBAction func getPhotoButtonAction(_ sender: UIButton) {
+        var config = PHPickerConfiguration()
+        config.selectionLimit = 1
+        
+        let pickerVC = PHPickerViewController(configuration: config)
+        pickerVC.delegate = self
+        self.present(pickerVC, animated: true)
+    }
+    
+}
+// MARK: - PHPickerViewControllerDelegate
+extension HomeViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        dismiss(animated: true)
+        
+        for result in results {
+            result.itemProvider.loadObject(ofClass: UIImage.self) { object, error in
+                if let image = object as? UIImage {
+                    self.imageArray = image
+                }
+                
+                DispatchQueue.main.async { [self] in
+                    self.photoImageView.image = imageArray
+                }
+            }
+        }
+    }
 }
